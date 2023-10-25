dc4bbaa43d32e536a113b1946ed705cbfa426d72
helgacozzo <helgacozzo@gmail.com>
2023-10-25 17:32:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+//NUMERO-ARRAY
+// let emptyArray = [];
+// let numero = parseInt(prompt("Scrivi un numero"));
+// let sommaNumeri = 0;
+
+// if(isNaN(numero)){
+//     alert("devi inserire un numero");
+// }
+
+// while(sommaNumeri<50){
+//     let numero = parseInt(prompt("Scrivi un numero"));
+//     emptyArray.push(numero);
+//     sommaNumeri += numero;
+// }
+
+// alert("array pieno:" + " " + emptyArray);
+// alert("somma dei numeri:" + " " + sommaNumeri);
+
+
+
+//NUMERO STAMPATO
+// let n = parseInt(prompt("Inserisci un numero da 1 a 100"));
+
+// if(isNaN(n) || n<1 || n>100){
+//        alert("devi inserire un numero da 1 a 100");
+// }
+
+// for(let i = 1; i <= n; i++){
+//     arrayEmpty = [];
+
+// for (let k = 0; k < 10; k++){
+// arrayN = Math.floor(Math.random() * 100) + 1;
+// arrayEmpty.push(arrayN);
+// } 
+
+// console.log(arrayEmpty);
+// }
+
+
+
+//INVITATI ALLA FESTA
+// let listaInvitati = ["Marco", "Elisabetta", "Giovanni", "Luigi"]
+// let nomeInvitato = prompt("Qual'è il tuo nome?")
+
+// if(listaInvitati.indexOf(nomeInvitato)){
+//     alert("Non sei invitato alla festa!");
+// }else{
+//     alert("Sei invitato alla festa!")
+// }
+
+
+
+//CHIEDI NUMERO 4 CIFRE
+// let promptUtente = prompt("Dimmi un numero di 4 cifre");
+// let numero = parseInt(promptUtente);
+// let numeroString = numero.toString;
+
+// if(isNaN(numeroString) || numeroString.length !==4){
+//     alert("Non hai inserito un numero di 4 cifre");
+// }else{
+
+//     let somma = 0;
+//     for(let i=0; i<4; 1++){
+//         let char = numeroString[i];
+//         somma = somma + Number(char);
+//     }
+// }
+
+
+
